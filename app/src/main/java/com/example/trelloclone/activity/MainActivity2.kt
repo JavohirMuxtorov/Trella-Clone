26bee56c7fa72ff4be8f7331b19a997947050671
Javohirbek <muxtorovj74@gmail.com>
2023-01-01 21:23:09 +0500

    committ

---
+++ app/src/main/java/com/example/trelloclone/activity/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.trelloclone.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trelloclone.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
